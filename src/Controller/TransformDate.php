a578612d9e7854a88437cdba0864086596f45723
Ali-Khakpash <ali.khakpash@gmail.com>
2019-08-31 00:08:09 +0430

    Fifth Commit

--- src/Controller/TransformDate.php
+++ src/Controller/TransformDate.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Symfony\Component\Routing\Annotation\Route;
+use App\CustomFunctions\gregorian2jalali;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 use Symfony\Component\Finder\Finder;
@@ -18,76 +19,24 @@ use Symfony\Component\Finder\Finder;
 class TransformDate extends AbstractController
 {
 
-
-
-    /**
-     * @var DateConverter
-     */
-  //  protected $dateConverter;
-
-    /**
-     * @var string
-     */
-   // protected $serverFormat;
-
-    /**
-     * @var string
-     */
-   // protected $locale;
-
-
-    // private $dateConverter;
-
-  /*  public function __construct(DateConverter $dateConverter)
-    {
-        $this->transformer = $dateConverter;
-    }*/
-
+     private $test;
 
     /**
      * @Route("/convertdate", name="convertdate", methods={"GET"})
      */
 
-/*    public function getLocale()
-    {
-        return $this->locale;
-    }*/
-
-/*    public function getCalendar()
-    {
-        return $this->calendar;
-    }*/
-
-
     public function transformdate (): Response
     {
-       // $this->calendar = $locale == 'fa' ? 'persian' : 'gregorian';
-        //$result = $this->dateConverter->georgianToPersian($gDate, $this->serverFormat, $this->getLocale(), $this->getCalendar(), false);
 
-  /*      $finder = new Finder();
-        $finder->files()->in('/hghgh');
-        //$finder->
 
-        if ($finder->hasResults()) {
-            return new  Response('<h3> There are so many files </h3>');
-        }
+        $this->test;
+        $jalali = new gregorian2jalali;
+        $jalali->mydate = "2007-02-13-1";
+        $jal = $jalali->gregorian_to_jalali("yy/mm/dd");
 
-        else{
-            return new  Response('<h3> There are no files </h3>');
-        }*/
 
-/*        foreach ($finder as $file) {
-            $absoluteFilePath = $file->getRealPath();
-            $fileNameWithExtension = $file->getRelativePathname();
-
-            // ...
-        }*/
-
-     //$jalali = new NarmafzamDateTransformer($dateConverter, $serverFormat, $locale) ;
-    // $jalali = $this->dateConverter->georgianToPersian('2019-08-28', $this->serverFormat, $this->getLocale(), $this->getCalendar(), false);
-      //echo $jalali;
         return new Response(
-            'There are no jobs in the database',
+            'Date:'.$jal,
             Response::HTTP_OK
         );
 
