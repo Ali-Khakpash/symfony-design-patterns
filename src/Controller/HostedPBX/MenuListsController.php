a9bccd8b722e577c21e711d61c9a2cf9f81a779d
Ali-Khakpash <ali.khakpash@gmaol.com>
2019-09-24 09:34:37 +0330

    Maybe The Last

--- src/Controller/HostedPBX/MenuListsController.php
+++ src/Controller/HostedPBX/MenuListsController.php
@@ -9,6 +9,10 @@ use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 
 /**
@@ -24,25 +28,85 @@ class MenuListsController extends AbstractController
     public function index(): Response
     {
         $client = HttpClient::create();
-        $response = $client->request('GET', 'http://172.25.17.111:8080/extensions');
+        $response = $client->request('GET', 'http://172.25.17.111:5000/api/v2.0/extension');
 
         $statusCode = $response->getStatusCode();
-        // $statusCode = 200
         $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
         $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-       // $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
         $content = json_decode( $content, true);
-        $new = json_decode($content['detail'],true);
         return $this->render('HostedPBX/Menu/extension.html.twig', [
-                'resp' => $new
+                'resp' => $content
         ]);
 
     }
 
 
+    /**
+     * @Route("/postextention", name="postextention", methods={"POST"})
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function post_extention(Request $request): Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('POST', 'http://172.25.17.111:5000/api/v2.0/extension', [
+
+            // defining data using an array of parameters
+            'body' => ['effective_caller_id_name' => $request->get('effective_caller_id_name'),
+                'effective_caller_id_number' => $request->get('effective_caller_id_number'),
+                'limit_max' => $request->get('limit_max'),
+                'limit_destination' => $request->get('limit_destination'),
+                'missed_call_app' => $request->get('missed_call_app'),
+                'user_record' => $request->get('user_record'),
+                'call_timeout' => $request->get('call_timeout'),
+                'call_group' => $request->get('call_group'),
+                'hold_music' => $request->get('hold_music'),
+/*                'call_timeout' => $request->get('call_timeout'),
+                'call_timeout' => $request->get('call_timeout'),
+                'call_timeout' => $request->get('call_timeout'),
+                'call_timeout' => $request->get('call_timeout'),*/
+            ]
+        ]);
+        $statusCode = $response->getStatusCode();
+
+        try {
+            $content = $response->getContent();
+        } catch (ClientExceptionInterface $e) {
+        } catch (RedirectionExceptionInterface $e) {
+        } catch (ServerExceptionInterface $e) {
+        } catch (TransportExceptionInterface $e) {
+        }
+       // $content = json_decode($content);
+        return new Response($content);
+
+    }
+
+
+
+    /**
+     * @Route("/deleteextension/{uuid}", name="deleteextension", methods={"GET","POST"})
+     */
+    public function delete_extension (String $uuid , Request $request): Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('DELETE', 'http://172.25.17.111:5000/api/v2.0/extension/'.$uuid);
+        return $this->redirectToRoute('extension');
+
+    }
+
+
+
+
+    /**
+     * @Route("/IVR", name="ivr", methods={"GET","POST"})
+     */
+    public function show_ivr(): Response
+    {
+
+        return $this->render('HostedPBX/Menu/ivr.html.twig', [
+            /* 'resp' => $new*/
+        ]);
+
+    }
 
 
 }
